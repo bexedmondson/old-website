84e0dbff69e5dc5006b90f9f4a6316f4b74b50c2
Hannah Woodward <hannah@builtbybuffalo.com>
2015-09-25 14:52:04 +0100

    more slick updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,3 @@
-/*$(document).ready(function() {
-    var slider = $(".slider").lightSlider({
-        item: 1,
-        slideMove: 1,
-        slideMargin: 0,
-        prevHtml: '<img src="/assets/bullet.svg">',
-        nextHtml: '<img src="/assets/bullet.svg">',
-        vertical:true,
-        verticalHeight:'100%',
-        pager: false,
-        currentPagerPosition: 'first',
-        enableDrag: false,
-        responsive: [
-            {
-                breakpoint:770,
-                settings: {
-                    vertical:false,
-                    //adaptiveHeight: true,
-                }
-            }
-        ]
-    });
-    $(window).resize(function(){
-        if
-    });
-});*/
 $(document).ready(function(){
   $('.slider').slick({
     accessibility: false,
